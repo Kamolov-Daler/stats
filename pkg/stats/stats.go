accd9fb8a8c23fc009c0bf723e2f6c4d7f41de20
Daler <>
2021-04-04 12:24:40 +0300

    added function periods  dynamic

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -48,3 +48,15 @@ func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
 	}
 	return categoriesPayment
 }
+
+func PeriodsDynamic(
+	first map[types.Category]types.Money, second map[types.Category]types.Money,
+) map[types.Category]types.Money {
+	result := map[types.Category]types.Money{}
+
+	for key := range second {
+		result[key] = second[key] - first[key]
+	}
+
+	return result
+}
